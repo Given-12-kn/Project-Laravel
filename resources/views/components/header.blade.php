3ac45e075b16bdcb0ba0c735acb3ff9b3e90498e
egyw <152834874+egyw@users.noreply.github.com>
2024-11-27 22:35:41 +0700

    nambah buat navbar
    nambah beberapa animasi

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -5,29 +5,28 @@
                 <img src="{{ asset('images/logo.png') }}" alt="spotify" width="50" height="45">
             </a>
       
-            
             <form class="d-flex ms-auto me-auto" role="search" style="height: 48px; width: 500px">
                 <div class="container-fluid d-flex container-home" style="width: 80px; height: 48px">
-                  <button class="btn-home rounded-circle" type="submit">
+                  <button class="btn-home rounded-circle">
                     <img src="{{ asset('images/home.svg' )}}" alt="home">
                   </button>
                 </div>
                 <div class="input-group">
-                  <button class="btn btn-search" type="submit">
+                  <button class="btn btn-search">
                     <img src="{{ asset('images/search.svg' )}}" alt="browse">
                   </button>
                   <input class="form-control search" type="search" placeholder="What do you want to play?" aria-label="Search">
-                  <button class="btn btn-browse" type="submit">
+                  <button class="btn btn-browse">
                     <img src="{{ asset('images/divider.svg' )}}" alt="divider" style="transform: rotate(90deg)">
                     <img src="{{ asset('images/browse.svg' )}}" alt="browse">
                   </button>
                 </div>
-          </form>
+            </form>
       
           <div class="d-flex">
-            <img src="{{ asset('images/notification.svg') }}" alt="notification">
-            <img src="{{ asset('images/friend.svg') }}" alt="friend" style="margin-left: 10px">
-            <img src="{{ asset('images/profile.svg') }}" class="rounded-circle" alt="profile" style="margin-left: 18px">
+            <img class="btn-topright" src="{{ asset('images/notification.svg') }}" alt="notification">
+            <img class="btn-topright" src="{{ asset('images/friend.svg') }}" alt="friend" style="margin-left: 15px">
+            <img class="btn-profile" src="{{ asset('images/profile.svg') }}" class="rounded-circle" alt="profile" style="margin-left: 20px">
           </div>
         </div>
     </nav>
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -19,42 +19,5 @@
 
     
 
-
-    <script>
-        const searchBtn = document.querySelector('.btn-search');
-const browseBtn = document.querySelector('.btn-browse');
-const searchImg = searchBtn.querySelector('.search-img');
-const browseImg = browseBtn.querySelector('.browse-img');
-
-// Menyimpan gambar default untuk search dan browse
-const searchImgDefault = '{{ asset('images/search.svg' )}}';
-const searchImgHover = '{{ asset('images/search-hover.svg' )}}'; // Ganti dengan path gambar saat hover
-const browseImgDefault = '{{ asset('images/browse.svg' )}}';
-const browseImgHover = '{{ asset('images/browse-hover.svg' )}}'; // Ganti dengan path gambar saat hover
-
-// Fungsi untuk mengganti gambar saat hover
-function changeSearchImgOnHover() {
-  searchImg.src = searchImgHover;
-}
-
-function revertSearchImg() {
-  searchImg.src = searchImgDefault;
-}
-
-function changeBrowseImgOnHover() {
-  browseImg.src = browseImgHover;
-}
-
-function revertBrowseImg() {
-  browseImg.src = browseImgDefault;
-}
-
-// Menambahkan event listener untuk hover pada tombol search dan browse
-searchBtn.addEventListener('mouseenter', changeSearchImgOnHover);
-searchBtn.addEventListener('mouseleave', revertSearchImg);
-
-browseBtn.addEventListener('mouseenter', changeBrowseImgOnHover);
-browseBtn.addEventListener('mouseleave', revertBrowseImg);
-    </script>
 </body>
 </html>
