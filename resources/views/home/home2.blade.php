d0a5e166ac56f5c0420ecbf7276bfe10c698528a
Jasonn <148473173+JasonFrz@users.noreply.github.com>
2025-01-05 18:57:57 +0700

    footer

--- resources/views/home/home2.blade.php
+++ resources/views/home/home2.blade.php
@@ -3,6 +3,7 @@
 @section('title', 'Home')
 
 @section('content')
+<script src="https://unpkg.com/ionicons@4.5.10-0/dist/ionicons.js"></script>
 <nav class="bg-gray-100">
   <div class="mx-auto max-w-7xl px-2 md:px-6 lg:px-8">
     <div class="relative flex h-20 items-center justify-between">
@@ -111,6 +112,49 @@
     <!-- Main content here -->
   </div>
 </div>
+
+<footer>
+  <div class="row">
+      <div class="col">
+          <img src="yoshi.png" class="footer_logo">
+          <p class="footer_about">given gay
+          </p>
+      </div>
+      <div class="col">
+          <h3>Office <div class="bottom_line"><span></span></div></h3>
+          <p>2841 Romines Mill Road</p>
+          <p>Plano</p>
+          <p>Texas, ZIP 75074, United States</p>
+          <p class="footer_email">alexastockphotos@gamil.com</p>
+          <h4>+1 - 2145958195</h4>
+      </div>
+      <div class="col">
+          <h3>Links <div class="bottom_line"><span></span></div></h3>
+          <ul>
+              <li><a href="">HOME</a></li>
+              <li><a href="">ABOUT</a></li>
+              <li><a href="">SERVICE</a></li>
+              <li><a href="">CONTACT US</a></li>
+          </ul>
+      </div>
+      <div class="col">
+          <h3>Newsletter <div class="bottom_line"><span></span></div></h3>
+          <form>
+              <ion-icon class="icon" name="mail"></ion-icon>
+              <input type="email" placeholder="Enter your email" required>
+              <button type="submit"><ion-icon class="icon_right" name="arrow-round-forward"></ion-icon></button>
+          </form>
+          <div class="social_icons">
+              <ion-icon class="social_icon" name="logo-facebook"></ion-icon>
+              <ion-icon class="social_icon" name="logo-whatsapp"></ion-icon>
+              <ion-icon class="social_icon" name="logo-twitter"></ion-icon>
+              <ion-icon class="social_icon" name="logo-instagram"></ion-icon>
+          </div>
+      </div>
+  </div>
+  <hr>
+  <p class="copyright">given gay Ⓒ 2022 - All Rights Reserved</p>
+</footer> 
 
 
 <script>
