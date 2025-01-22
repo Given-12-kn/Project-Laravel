7aebebaca0b468083a3b8588acc4aaa701c8c86d
egyw <152834874+egyw@users.noreply.github.com>
2025-01-22 20:13:08 +0700

    fix overflow admin panel

--- resources/views/layout/sideBar.blade.php
+++ resources/views/layout/sideBar.blade.php
@@ -60,12 +60,7 @@
         </aside>
 
         <!-- Content -->
-        <div id="content" class="transition-all duration-300 w-full">
-            <button id="openSidebarBtn" class="hidden bg-blue-500 p-2 rounded focus:outline-none md:hidden" onclick="showSidebar()">
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M4 6h16M4 12h16M4 18h16" />
-                </svg>
-            </button>
+        <div id="content" class="w-full max-h-screen overflow-y-auto overflow-x-hidden transition-all duration-300">
             @yield('content-sidebar')
         </div>
     </div>
